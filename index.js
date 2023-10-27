ef493f877441378a99e435ea7842e0a135fef3c6
Gustavo Marconi <gustavo.marconi@redkite.com>
2023-10-27 14:46:07 -0300

    update server

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv")
 const axios = require('axios')
-const {
-  createHmac,
-} = require('node:crypto');
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const SLACK_TOKEN = process.env.SLACK_TOKEN || ''
-const SLACK_SIGNIN_SECRET = process.env.MY_SLACK_SIGNING_SECRET;
 
 const app = express()
 app.use(cors({
@@ -35,17 +31,6 @@ app.get('/api/slack', async (req, res) => {
 
 app.post('/api/slack/events', (req, res) => {
   console.log(req.body);
-
-  // const body = req.body();
-  // const version = 'v0';
-  // const signInSecret = SLACK_SIGNIN_SECRET;
-  // const timestamp = req.headers['X-Slack-Request-Timestamp']
-
-  // const baseString = `${version}:${timestamp}:${body}`;
-
-  // const hmac = createHmac('sha256', signInSecret);
-
-  // const my_signature = 'v0=' + hmac.update(baseString).digest()
 
   res.send(req.body.challenge);
   res.end();
