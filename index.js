f5ee4f17cf49d6433528ca63f985c58ad5986af2
Gustavo Marconi <gustavo.marconi@redkite.com>
2023-10-30 13:07:16 -0300

    adding chatgpt

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv")
 const axios = require('axios')
+const OpenAI = require("openai");
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const SLACK_TOKEN = process.env.SLACK_TOKEN || '' // if we need to
 const BOT_TOKEN = process.env.SLACK_BOT_TOKEN || ''
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ''
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const app = express()
 app.use(cors({
@@ -48,9 +54,38 @@ app.post('/api/slack/events', async (req, res) => {
 });
 
 app.post('/api/slack/slash', async (req, res) => {
-  res.send('We will see what we can find')
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: req.body }],
+      model: "gpt-3.5-turbo",
+    });
+    const [choice] = chatCompletion.choices;
+    res.send(choice.message);
+  
+  } catch(error) {
+    console.log(error)
+  }
   res.end();
 });
+
+app.get('/api/slack/chat', async (req, res) => {
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: "Who are the last 10 FIFA world cup champtions" }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log('deu')
+    chatCompletion.choices.map(choice => console.log(choice))
+  
+  } catch(error) {
+    console.log(error)
+  }
+  res.send('ok')
+
+  res.end();
+  
+  
+})
 
 app.listen(PORT, function () {
   console.log(`Express app listening on port ${PORT}`)
